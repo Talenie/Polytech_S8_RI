a6bdf5dfd11bbd575cf7f868e623efcbd7b86125
Talenie <alicia.abonnenc@hotmail.com>
2017-03-10 12:21:51 +0100

    code plus propre qui crée les chemins et ajout d'un script de nettoyage

---
+++ Main/clean.sh
@@ -0,0 +1,1 @@
+rm -r Docs Filtre STP RepBi RepFq Query FiltreQ STPQ RepBiQ RepFqQ vocabulaire document_frequency
--- Main/main.pl
+++ Main/main.pl
@@ -1,22 +1,28 @@
 #############
 # FONCTIONS #
 #############
-sub ExtractionDesFichiers($$)
+
+# Parametres : Nom du fichier d'extraction, Chemin des fichiers, nom  des fichiers
+sub ExtractionDesFichiers($$$)
 {
-   my ($FileName,$Path)=@_; # Tableau des paramètres 
+   my ($FileName,$Path,$Name)=@_; # Tableau des paramètres 
    open(F,$FileName) || die "Erreur d'ouverture du fichier $FileName\n";
    my $str="";
    my $Num=0;
-
+	
+	# Si le $Path n'existe pas on le crée
+	if (!(-d $Path)){
+		system("mkdir $Path");
+	}
    open(COL,">$Path/Collection") || die "Erreur de creation de Collection\n";
    while(!eof(F)){
      if($str =~m /\.I\s/){ # On regarde si s$tr contient la chaîne .I
         close(NF);
         $str =~s/\.I\s//g; # Dans $str, on supprime la chaîne .I avant le numéro de document
         $Num=$str;
-        print COL "CACM-$Num\n";
-        print "Processing ... CACM-$Num\n";
-        open(NF,">$Path/CACM-$Num");
+        print COL "$Name-$Num\n";
+        #print "Processing ... $Name-$Num\n";
+        open(NF,">$Path/$Name-$Num");
      }
      if(($str=~ m/\.T/) || ($str=~ m/\.A/) || ($str=~ m/\.W/) || ($str=~ m/\.B/)) { # Si $str contient une des balises que l'on veut 
         $Go=1;
@@ -38,54 +44,26 @@ sub ExtractionDesFichiers($$)
   close(F);
 }
 
-sub ExtractionDesQuerys($$)
+# Enleve les caractère alphanumériques
+# Paramètres : Chemin des fichiers en entrée, leur nom, le chemin de sortie et le nombre à traiter
+sub FiltrerAlphanum($$$$)
 {
-   my ($FileName,$Path)=@_; # Tableau des paramètres 
-   open(F,$FileName) || die "Erreur d'ouverture du fichier $FileName\n";
-   my $str="";
-   my $Num=0;
-
-   open(COL,">$Path/Collection") || die "Erreur de creation de Collection\n";
-   while(!eof(F)){
-     if($str =~m /\.I\s/){ # On regarde si s$tr contient la chaîne .I
-        close(NF);
-        $str =~s/\.I\s//g; # Dans $str, on supprime la chaîne .I avant le numéro de document
-        $Num=$str;
-        print COL "Query-$Num\n";
-        print "Processing ... Query-$Num\n";
-        open(NF,">$Path/Query-$Num");
-     }
-     if(($str=~ m/\.T/) || ($str=~ m/\.A/) || ($str=~ m/\.W/) || ($str=~ m/\.B/)) { # Si $str contient une des balises que l'on veut 
-        $Go=1;
-        while($Go==1){  # Tant que l'on ne rencontre pas une nouvelle balise
-           chop($str=<F>);
-           if(($str eq "\.W") || ($str eq "\.B") || ($str eq "\.N") || ($str eq "\.A") || ($str eq "\.X") || ($str eq "\.K") || ($str eq "\.T") || ($str eq "\.I")){
-             $Go=0;
-             break;
-           }
-           else{
-             print NF "$str "; # On écrit le contenu dans le fichier CACM-XX
-           }
-        }
-     }
-     else{
-       chop($str=<F>);
-     }
-  }
-  close(F);
-}
-
-sub FiltrerAlphanum($)
-{
-	my($Path)= @_;
+	my($PathIn,$Name,$PathOut,$num)= @_;
 	my $i = 1;
 	my $str="1";
-    while($i <= 3204){
-		open(FS,"$Path/CACM-$i") || die "Erreur d'ouverture du fichier CACM-$i\n";
-		open(FLT,">Filtre/CACM-$i.flt") || die "Erreur de creation de CACM-$i.flt\n";
+	
+	# Si le $Path n'existe pas on le crée
+	if (!(-d $PathOut)){
+		system("mkdir $PathOut");
+	}
+	
+    while($i <= $num){
+		open(FS,"$PathIn/$Name-$i") || die "Erreur d'ouverture du fichier $Name-$i\n";
+		open(FLT,">$PathOut/$Name-$i.flt") || die "Erreur de creation de $Name-$i.flt\n";
 		
 		while(chop($str=<FS>)){
 			$str = lc( $str );
+			# on applique le filtre
 			$str =~ s/(\,|\=|\/|\.|\?|\'|\(|\)|\_|\$|\%|\+|\[|\]|\{|\}|\&|\;|\:|\~|\!|\@|\#|\^|\*|\||\<|\>|\-|\\s|\\|\")//g;
 			print FLT "$str";
 		}
@@ -95,19 +73,42 @@ sub FiltrerAlphanum($)
 	}
 }
 
-sub FiltrerAlphanumQuery($)
+# Enleve des fichier les mots présent dans StopFile
+# Paramètres : Chemin, nom des fichies, nombre de fichiers, fichier avec les mots,  chemin en sortie
+sub StopWords($$$$$)
 {
-	my($Path)= @_;
+	my($PathIn,$Name,$Num,$StopFile,$PathOut)= @_;
 	my $i = 1;
 	my $str="1";
-    while($i <= 64){
-		open(FS,"$Path/Query-$i") || die "Erreur d'ouverture du fichier Query-$i\n";
-		open(FLT,">FiltreQ/Query-$i.flt") || die "Erreur de creation de Query-$i.flt\n";
+	
+	
+	open(FST,"$StopFile") || die "Erreur ouverture de $StopFile";
+	
+	my %stop_words;
+	
+	while(chop($word=<FST>)){
+		$stop_words{$word} = 1;
+	}
+	
+	# Si le $Path n'existe pas on le crée
+	if (!(-d $PathOut)){
+		system("mkdir $PathOut");
+	}
+	
+	
+    while($i <= $Num){
+		open(FS,"$PathIn/$Name-$i.flt") || die "Erreur d'ouverture du fichier $PathIn/$Name-$i.flt\n";
+		open(FLT,">$PathOut/$Name-$i.stp") || die "Erreur de creation de $Name-$i.plt\n";
 		
-		while(chop($str=<FS>)){
-			$str = lc( $str );
-			$str =~ s/(\,|\=|\/|\.|\?|\'|\(|\)|\_|\$|\%|\+|\[|\]|\{|\}|\&|\;|\:|\~|\!|\@|\#|\^|\*|\||\<|\>|\-|\\s|\\|\")//g;
-			print FLT "$str";
+		while(<FS>){
+			for $mot (split){
+				if (exists $stop_words{$mot}){
+					#print "Enleve : $mot\n";
+				} else {
+					#print "Garde : $str\n";
+					print FLT "$mot ";
+				}
+			}
 		}
 		close(F);
 		close(FLT);
@@ -115,9 +116,10 @@ sub FiltrerAlphanumQuery($)
 	}
 }
 
-sub createVoc($)
+# Crée un vocabulaire a partir des fichiers dans Path et les place dans Name
+sub createVoc($$)
 {
-	my($Path)= @_;
+	my($Path,$Name)= @_;
 	my $i = 1;
 	my $str="1";
 	
@@ -136,93 +138,23 @@ sub createVoc($)
 		$i = $i +1;
 	}
 	
-	open(FV,">vocabulaire") || die "Erreur de creation de vocabulaire\n";
+	open(FV,">$Name") || die "Erreur de creation de $Name\n";
 	
 	while( my ($mot, $val) = each %voc){
 		print FV "$mot\n";
 	}
 }
 
-sub StopWords($$)
+# Crée la DF d'une collection de fichiers
+sub create_df($$$$$)
 {
-	my($Path,$StopFile)= @_;
-	my $i = 1;
-	my $str="1";
-	
-	
-	open(FST,"$StopFile") || die "Erreur ouverture de $StopFile";
-	
-	my %stop_words;
-	
-	while(chop($word=<FST>)){
-		$stop_words{$word} = 1;
-	}
-	
-    while($i <= 3204){
-		open(FS,"$Path/CACM-$i.flt") || die "Erreur d'ouverture du fichier $Path/CACM-$i.flt\n";
-		open(FLT,">STP/CACM-$i.stp") || die "Erreur de creation de CACM-$i.plt\n";
-		
-		while(<FS>){
-			for $mot (split){
-				if (exists $stop_words{$mot}){
-					#print "Enleve : $mot\n";
-				} else {
-					#print "Garde : $str\n";
-					print FLT "$mot ";
-				}
-			}
-		}
-		close(F);
-		close(FLT);
-		$i = $i +1;
-	}
-}
-
-sub StopWordsQuery($$)
-{
-	my($Path,$StopFile)= @_;
-	my $i = 1;
-	my $str="1";
-	
-	
-	open(FST,"$StopFile") || die "Erreur ouverture de $StopFile";
-	
-	my %stop_words;
-	
-	while(chop($word=<FST>)){
-		$stop_words{$word} = 1;
-	}
-	
-    while($i <= 64){
-		open(FS,"$Path/Query-$i.flt") || die "Erreur d'ouverture du fichier $Path/Query-$i.flt\n";
-		open(FLT,">STPQ/Query-$i.stp") || die "Erreur de creation de Query-$i.plt\n";
-		
-		while(<FS>){
-			for $mot (split){
-				if (exists $stop_words{$mot}){
-					#print "Enleve : $mot\n";
-				} else {
-					#print "Garde : $str\n";
-					print FLT "$mot ";
-				}
-			}
-		}
-		close(F);
-		close(FLT);
-		$i = $i +1;
-	}
-}
-
-
-sub create_df($$)
-{
-	my($Path,$vocFile)= @_;
+	my($Path,$Name,$Num,$vocFile,$outFile)= @_;
 	my $i = 1;
 	
 	my %voc;
 		
-    while($i <= 3204){
-		open(FS,"$Path/CACM-$i.stp") || die "Erreur d'ouverture du fichier $Path/CACM-$i.stp\n";
+    while($i <= $Num){
+		open(FS,"$Path/$Name-$i.stp") || die "Erreur d'ouverture du fichier $Path/$Name-$i.stp\n";
 		my %doc;
 		while(<FS>){
 			for $mot (split){
@@ -242,7 +174,7 @@ sub create_df($$)
 		$i = $i +1;
 	}
 	
-	open(FDF,">document_frequency") || die "Erreur de creation de document_frequency\n";
+	open(FDF,">$outFile") || die "Erreur de creation de $outFile\n";
 	
 	while( my ($mot, $val) = each %voc){
 		print FDF "$val $mot\n";
@@ -251,13 +183,16 @@ sub create_df($$)
 	close(FDF);
 }
 
-sub representation_binaire($$)
+
+
+
+sub representations($$$$$$)
 {
-	my($file_voc,$Path)= @_;
+	my($Voc,$PathIn,$Name,$Num,$PathBi,$PathFq)= @_;
 		
 	my %voc;
 	
-	open(FS,"$file_voc") || die "Erreur d'ouverture du fichier $file_voc\n";
+	open(FS,"$Voc") || die "Erreur d'ouverture du fichier $Voc\n";
 	my $i = 1;
 	while(<FS>){
 		for $mot (split){
@@ -267,10 +202,18 @@ sub representation_binaire($$)
 	}
 	close(FS);
 	
+	# Si le $Path n'existe pas on le crée
+	if (!(-d $PathBi)){
+		system("mkdir $PathBi");
+	}
+	# Si le $Path n'existe pas on le crée
+	if (!(-d $PathFq)){
+		system("mkdir $PathFq");
+	}
 	
 	$i = 1;
-	while($i <= 3204){
-		open(F,"$Path/CACM-$i.stp") || die "Erreur d'ouverture du fichier $Path/CACM-$i.stp\n";
+	while($i <= $Num){
+		open(F,"$PathIn/$Name-$i.stp") || die "Erreur d'ouverture du fichier $PathIn/$Name-$i.stp\n";
 		my %rep;
 		my %rep_fq;
 		while(<F>){
@@ -282,7 +225,7 @@ sub representation_binaire($$)
 		}
 		close(F);
 		
-		open(FREP,">Representations/CACM-$i.bi") || die "Erreur de creation du fichier Representations/CACM-$i.bi\n";
+		open(FREP,">$PathBi/$Name-$i.bi") || die "Erreur de creation du fichier $PathBi/$Name-$i.bi\n";
 		foreach my $indice (sort {$a <=> $b} keys %rep) {
 			print FREP "$indice:$rep{$indice}\n";
 		}
@@ -290,7 +233,7 @@ sub representation_binaire($$)
 		close(FREP);
 		
 		
-		open(FREQ,">Representations/CACM-$i.fq") || die "Erreur de creation du fichier Representations/CACM-$i.fq\n";
+		open(FREQ,">$PathFq/$Name-$i.fq") || die "Erreur de creation du fichier $PathFq/$Name-$i.fq\n";
 		foreach my $indice (sort {$a <=> $b} keys %rep) {
 			print FREQ "$indice:$rep_fq{$indice}\n";
 		}
@@ -354,15 +297,16 @@ sub representation_binaire_query($$)
 # MAIN PROG #
 #############
 
-ExtractionDesFichiers("cacm.all","Docs");
-FiltrerAlphanum("Docs");
-StopWords("Filtre","common_words");
-createVoc("STP");
-create_df("STP","vocabulaire");
-representation_binaire("vocabulaire","STP");
-ExtractionDesQuerys("query.text","Querys");
-FiltrerAlphanumQuery("Querys");
-StopWordsQuery("FiltreQ","common_words");
-representation_binaire_query("vocabulaire","STPQ");
+ExtractionDesFichiers("cacm.all","Docs","CACM");
+FiltrerAlphanum("Docs","CACM","Filtre",3204);
+StopWords("Filtre","CACM",3204,"common_words","STP");
+createVoc("STP","vocabulaire");
+create_df("STP","CACM",3204,"vocabulaire","document_frequency");
+representations("vocabulaire","STP","CACM",3204,"RepBi","RepFq");
+
+ExtractionDesFichiers("query.text","Querys","Query");
+FiltrerAlphanum("Querys","Query","FiltreQ",64);
+StopWords("FiltreQ","Query",64,"common_words","STPQ");
+representations("vocabulaire","STPQ","Query",64,"RepBiQ","RepFqQ");
 
 
