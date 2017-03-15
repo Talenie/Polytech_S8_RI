44f0ef4630028645ab5cea64260761a467d12e17
Gilles B <bonhoure.gilles@gmail.com>
2017-03-15 22:27:38 +0100

    question 4 traitée

--- TP5-6/cosine.pl
+++ TP5-6/cosine.pl
@@ -1,12 +1,12 @@
-sub cosine($$) {
+sub cosine($$$$) {
 	
-	my ($fdocument,$fquery)=@_; # Tableau des paramètres 
+	my ($fdocument, $id, $ext, $fquery)=@_; # Tableau des paramètres 
 	
-	#print "Running cosine $fdocument\n";
+	#print "Running cosine $fdocument/$id\n";
 	
 	my %doc;
 	my $sumSquareDoc = 0;
-	open(FD,"$fdocument") || die "Erreur d'ouverture du fichier $fdocument\n";
+	open(FD,"$fdocument/CACM-$id.$ext") || die "Erreur d'ouverture du fichier $fdocument/CACM-$id.$ext\n";
 	while(<FD>) {
 		for $line (split) {
 			@content = split(/(:)/, $line);
@@ -53,15 +53,17 @@ sub cosine($$) {
 	
 }
 
-sub rank($$$) {
+sub rank($$$$$) {
 
-	my($nb, $dir, $query) = @_;	
+	my($nb, $dir, $ext, $query, $fsortie) = @_;	
 	my @files = glob( $dir . '/*' );
 	
 	my %ranking;
 	
-	foreach my $filename (@files) {
-		$ranking{$filename} = cosine($filename, $query);
+	my $i=1;
+	while ($i<=3024) {
+		$ranking{$i} = cosine($dir, $i, $ext, $query);
+		$i++;
 	}
 	
 	my $rank = 0;
@@ -71,16 +73,59 @@ sub rank($$$) {
 		$reverseRanking{$ranking{$key}} = $key;
 	}
 	
-	print "rank	doc					proba\n";
+	open(FS,">$fsortie") || die "Erreur d'ouverture du fichier $fsortie\n";
+	#print "rank	doc					proba\n";
 	foreach my $value (reverse sort values %ranking) {
 		if($rank++ < $nb) {
 			$key = $reverseRanking{$value};
-			print "$rank.	$key	$value\n";
+			print FS "$rank.	$key	$value\n";
 		};
 	}
+	close(FS);
 	
 	
 }
 
+# Fonction permettant de calculer une précision moyenne à partir d'une requete sur des documents
+#
+# $fqrels l'adresse du fichier de référence
+# $queryNb le numéro de la requête
+# $nb le nombre de rangs à traiter
+# $dir le dossier des documents à traiter
+# $ext l'etenssion des fichiers à traiter
+# $query l'adresse du fichier de requête
+sub epk($$$$$$) {
+	my($fqrels, $queryNb, $nb, $dir, $ext, $query) = @_;
+	
+	# enregistrement des fichiers pertinents pour la requête en question
+	my %qrels;
+	open(FQ,"$fqrels") || die "Erreur d'ouverture du fichier $fqrels\n";
+	while ($line = <FQ>) {
+		@content = split(/( )/, $line);
+		if ($content[0] == $queryNb) {
+			$qrels{$content[2]} = $content[0];
+		}
+	}
+	close(FD);
+	
+	# choix du fichier de sauvegarde et calcul des similarités de documents
+	my $frank = ".temp.rank";
+	rank($nb, $dir, $ext, $query, $frank);
+	
+	my $matching = 0; # variable de concordance
+	open(FR, "$frank") || die "Erreur d'ouverture du fichier $frank\n";
+	while ($line = <FR>) {
+		@content = split(/(	)/, $line);
+		# si le document similaire est présent dans les document réellement cohérent avec la requête, on incrémente $matching
+		if($qrels{$content[2]}) {
+			$matching++;
+		}
+	}
+	close(FR);
+	
+	return $matching/$nb;	# retour de la valeur finale en fonction du nombre de document triés
+}
+
 #cosine($ARGV[0], $ARGV[1]);
-rank($ARGV[0], $ARGV[1], $ARGV[2]);
+#rank($ARGV[0], $ARGV[1], $ARGV[2], $ARGV[3], $ARGV[4]);
+epk($ARGV[0], $ARGV[1], $ARGV[2], $ARGV[3], $ARGV[4], $ARGV[5]);
