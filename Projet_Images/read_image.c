aba54f80a115517f08fb8ccc4bfa6fef3f873ed4
Talenie <alicia.abonnenc@hotmail.com>
2017-03-24 11:46:42 +0100

    MAde a makefile and modified histo

--- Projet_Images/read_image.c
+++ Projet_Images/read_image.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-  int i,j,n,nx,ny,nb;
+  int i,j;
   CIMAGE cim;
   /*------------------------------------------------*/
   /* lecture d'une image requête                    */
